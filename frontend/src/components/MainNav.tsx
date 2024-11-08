004bb435cf0cb44f5b962d0faa30f5d292d0ac58
hiroki <haohuirixia851@gmail.com>
2024-11-08 18:14:39 +0900

    build main navbar

---
+++ frontend/src/components/MainNav.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./ui/button";
+
+const MainNav = () => {
+  return (
+    <Button
+      className="font-bold hover:text-orange-500 hover:bg-white"
+      variant="ghost"
+    >
+      Log in
+    </Button>
+  );
+};
+
+export default MainNav;
