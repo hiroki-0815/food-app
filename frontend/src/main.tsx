04bf30b2e3af7317e9f36ca68f7f3ee89628ff5f
hiroki <haohuirixia851@gmail.com>
2024-11-09 11:31:04 +0900

    build athentification

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,10 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./global.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRouter from "./AppRouter";
+import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Router>
+      <Auth0ProviderWithNavigate>
+        <AppRouter />
+      </Auth0ProviderWithNavigate>
       <AppRouter />
     </Router>
   </StrictMode>
