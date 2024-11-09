0c6db7963550c1d13c8e2dfd685e686d509a537b
hiroki <haohuirixia851@gmail.com>
2024-11-09 20:13:08 +0900

    Create user request

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,13 +4,24 @@ import "./global.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRouter from "./AppRouter";
 import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Router>
-      <Auth0ProviderWithNavigate>
-        <AppRouter />
-      </Auth0ProviderWithNavigate>
+      <QueryClientProvider client={queryClient}>
+        <Auth0ProviderWithNavigate>
+          <AppRouter />
+        </Auth0ProviderWithNavigate>
+      </QueryClientProvider>
     </Router>
   </StrictMode>
 );
