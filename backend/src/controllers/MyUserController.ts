972d4a2a42604654e6b8b2b723f8b3dbda8a020e
hiroki <haohuirixia851@gmail.com>
2024-11-11 16:30:18 +0900

    Update user profile api

--- backend/src/controllers/MyUserController.ts
+++ backend/src/controllers/MyUserController.ts
@@ -2,7 +2,7 @@
 import { Request, Response, RequestHandler } from "express";
 import User from "../models/user";
 
-export const createCurrentUser: RequestHandler = async (req, res) => {
+export const createCurrentUser: RequestHandler = async (req: Request, res: Response) => {
   try {
     const { auth0Id } = req.body;
     const existingUser = await User.findOne({ auth0Id });
@@ -23,3 +23,27 @@ export const createCurrentUser: RequestHandler = async (req, res) => {
     return; 
   }
 };
+
+export const updateCurrentUser: RequestHandler = async(req: Request, res: Response): Promise<void> =>{
+  try {
+    const {name, addressLine1, country, city} =req.body;
+    const user = await User.findById(req.userId)
+
+    if(!user){
+       res.status(404).json({message: "User not found"})
+       return;
+    }
+
+    user.name = name;
+    user.addressLine1 = addressLine1;
+    user.city = city;
+    user.country =country;
+
+    await user.save();
+    res.send(user);
+    
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({message: "Error updating user"})
+  }
+}
--- backend/src/routes/MyUserRoute.ts
+++ backend/src/routes/MyUserRoute.ts
@@ -1,9 +1,12 @@
 import express from "express";
-import { createCurrentUser } from "../controllers/MyUserController";
-import { jwtCheck } from "../middleware/auth";
+import { createCurrentUser, updateCurrentUser } from "../controllers/MyUserController";
+import { jwtCheck, jwtParse } from "../middleware/auth";
+import { validateMyUserRequest } from "../middleware/validation";
 
 const router = express.Router();
 
 router.post("/",jwtCheck, createCurrentUser);
+router.put("/",jwtCheck, jwtParse, validateMyUserRequest, updateCurrentUser);
+
 
 export default router;
